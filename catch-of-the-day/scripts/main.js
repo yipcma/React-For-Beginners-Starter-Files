5f45049ea7768c6484c0ca8c6ad8ee453d8c7781
Andrew Yip <yipcma@gmail.com>
2016-05-13 22:30:05 +0300

    Persisting State with LocalStorage

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -31,8 +31,16 @@ var App = React.createClass({
       context: this,
       state: 'fishes'
     })
+    var localStorageRef = localStorage.getItem('order-'+ this.props.params.storeId)
+    if(localStorageRef) {
+      this.setState({order: JSON.parse(localStorageRef)});
+    }
   },
-  
+
+  componentWillUpdate: function(nextProps, nextState) {
+    localStorage.setItem('order-'+ this.props.params.storeId, JSON.stringify(nextState.order));
+  },
+
   loadSamples: function() {
   this.setState({fishes: require('./sample-fishes')})
   },
