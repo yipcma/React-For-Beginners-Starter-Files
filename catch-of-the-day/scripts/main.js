a7549632cc7e53be2a34f6db70763d2bb9cd289c
Andrew Yip <yipcma@gmail.com>
2016-05-13 03:21:09 +0300

    Writing HTML with JSX

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -6,9 +6,15 @@ var ReactDOM = require('react-dom');
 
 var StorePicker = React.createClass({
 
-  render : function() {
+  render: function() {
+    var name = "wes";
     return (
-      <p>hello</p>
+      <form className="store-selector">
+      {/* jsx comment here */}
+        <h2>Please enter a store, {name}</h2>
+        <input type="text" ref="storeId" required />
+        <input type="Submit" />
+      </form>
     );
   }
 });
