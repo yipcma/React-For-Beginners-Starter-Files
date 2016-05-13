ca7dc315ba19ae597ae7b510bf1698ad9e51d675
Andrew Yip <yipcma@gmail.com>
2016-05-13 03:10:35 +0300

    Writing your first Component with eslint

---
+++ catch-of-the-day/scripts/main.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+/*
+  StorePicker
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>hello</p>
+    );
+  }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
