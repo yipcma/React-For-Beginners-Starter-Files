24777ac73eb996c248eb36fbbec58b8c1a85ca85
Andrew Yip <yipcma@gmail.com>
2016-05-13 13:39:01 +0300

    Creating our application layout with components

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,5 +1,57 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+/*
+  App
+*/
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
+});
+
+/*
+  Header
+*/
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    );
+  }
+});
+
+/*
+  Order
+*/
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    );
+  }
+});
+
+/*
+  Inventory
+*/
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    );
+  }
+});
+
 /*
   StorePicker
 */
@@ -19,4 +71,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App />, document.querySelector('#main'));
