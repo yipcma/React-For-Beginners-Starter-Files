2e74d11588e1a963c5455e99cfe5ddea04229c3a
Andrew Yip <yipcma@gmail.com>
2016-05-13 16:05:28 +0300

    Routing with React Router

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
 
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 /*
   App
 */
@@ -79,4 +84,29 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+/*
+  Not-found
+*/
+
+var NotFound = React.createClass({
+
+   render: function() {
+     return (
+       <h1>Not Found</h1>
+     );
+   }
+});
+
+/*
+  Routers
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
